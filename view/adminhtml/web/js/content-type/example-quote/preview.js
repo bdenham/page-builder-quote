cad6b1efcfc036ce6b7678123803d53d66142170
Dave Macaulay <macaulay@adobe.com>
2019-02-25 16:55:12 +0100

    Add stage image upload capabilities

--- view/adminhtml/web/js/content-type/example-quote/preview.js
+++ view/adminhtml/web/js/content-type/example-quote/preview.js
@@ -1,131 +1,137 @@
 define([
-        'Magento_PageBuilder/js/content-type/preview',
-        'Magento_PageBuilder/js/content-type-toolbar',
-        'Magento_PageBuilder/js/events',
-    ], function (PreviewBase, Toolbar, events) {
-        'use strict';
+    'Magento_PageBuilder/js/content-type/preview',
+    'Magento_PageBuilder/js/content-type-toolbar',
+    'Magento_PageBuilder/js/events',
+    'Magento_PageBuilder/js/uploader'
+], function (PreviewBase, Toolbar, events, Uploader) {
+    'use strict';
 
-        function Preview(parent, config, stageId) {
-            PreviewBase.call(this, parent, config, stageId);
-            this.toolbar = new Toolbar(this, this.getToolbarOptions());
-        }
-
-        var $super = PreviewBase.prototype;
-
-        Preview.prototype = Object.create(PreviewBase.prototype);
-
-        Preview.prototype.bindEvents = function bindEvents() {
-            var self = this;
-            // console.log(self);
-            PreviewBase.prototype.bindEvents.call(this);
-
-            // events.on(`${this.config.name}:${this.contentType.id}:updateAfter`, function (args) {
-            //     console.log("UpdateAfter: " + self.contentType);
-            // });
-
-            // events.on(`${this.config.name}:${this.contentType.id}:updateAfter`, function (args) {
-            //     var ct = Object.values(args)[0].config;
-            //     console.log(ct);
-            //     console.log(self.config);
-            // });
-
-            // events.on("column:initializeAfter", function (args) {
-            //     console.log(args);
-            // });
-
-            // events.on(`${self.config.name}:moveBefore`, function (args) {
-            //     self.handleEvent(args);
-            // });
-
-            // events.on(`contentType:renderAfter`, function (args) {
-            //     // console.log(args.contentType.config.name);
-            //     if (args.contentType.config.name === "image") {
-            //         self.handleEvent(args);
-            //     }
-            // });
-
-            // events.on("example_quote:mountAfter", function (args) {
-            //     console.log(args);
-            //     // console.log(args);
-            // });
-
-            events.on("stage:" + self.contentType.stageId + ":fullScreenModeChangeAfter", function (args) {
-                console.log(args);
-            });
-        };
-
-        Preview.prototype.handleEvent = function (args) {
-            console.log("Binding Works");
-        };
-
-        Preview.prototype.retrieveOptions = function retrieveOptions() {
-            var options = $super.retrieveOptions.call(this, arguments);
-            //console.log(options);
-
-            // Change option menu icons
-            options.remove.icon = "<i class='icon-admin-pagebuilder-error'></i>";
-
-            // Change tooltips
-            options.edit.title = "Open Editor";
-            // options.remove.title = "Remove";
-            // options.move.title = "Move";
-            // options.duplicate.title = "Duplicate";
-
-            // Remove menu options
-            // delete options.move;
-            // delete options.duplicate;
-            // delete options.edit;
-            // delete options.remove;
-
-            return options;
-        };
-
-        Preview.prototype.getToolbarOptions = function getToolbarOptions() {
-            return [
-                {
-                    key: "text_align",
-                    type: "select",
-                    values: [{
-                        value: "left",
-                        label: "Left",
-                        icon: "icon-pagebuilder-align-left"
-                    }, {
-                        value: "center",
-                        label: "Center",
-                        icon: "icon-pagebuilder-align-center"
-                    }, {
-                        value: "right",
-                        label: "Right",
-                        icon: "icon-pagebuilder-align-right"
-                    }]
-                },
-                {
-                    key: "quote_css",
-                    type: "select",
-                    values: [{
-                        value: "black-quote",
-                        label: "Black",
-                        icon: "icon-pagebuilder-black-swatch"
-                    }, {
-                        value: "blue-quote",
-                        label: "Blue",
-                        icon: "icon-pagebuilder-blue-swatch"
-                    }, {
-                        value: "green-quote",
-                        label: "Green",
-                        icon: "icon-pagebuilder-green-swatch"
-                    }, {
-                        value: "red-quote",
-                        label: "Red",
-                        icon: "icon-pagebuilder-red-swatch"
-                    }, {
-                        value: "purple-quote",
-                        label: "Purple",
-                        icon: "icon-pagebuilder-purple-swatch"
-                    }]
-                }
-            ];
-        };
-        return Preview;
+    function Preview(parent, config, stageId) {
+        PreviewBase.call(this, parent, config, stageId);
+        this.toolbar = new Toolbar(this, this.getToolbarOptions());
     }
-);
+
+    var $super = PreviewBase.prototype;
+
+    Preview.prototype = Object.create(PreviewBase.prototype);
+
+    /**
+     * Bind any events required for the content type to function
+     */
+    Preview.prototype.bindEvents = function () {
+        var self = this;
+        PreviewBase.prototype.bindEvents.call(this);
+
+        events.on(
+            "stage:" + self.contentType.stageId + ":fullScreenModeChangeAfter",
+            this.handleEvent.bind(this)
+        );
+    };
+
+    /**
+     * An example callback from the above bound event
+     *
+     * @param args
+     */
+    Preview.prototype.handleEvent = function (args) {
+        console.log("Binding Works");
+    };
+
+    /**
+     * Return a new instance of the uploader to allow for inline image uploading capabilities
+     *
+     * @returns {*}
+     */
+    Preview.prototype.getUploader = function () {
+        var initialImageValue = this.contentType.dataStore
+            .get(this.config.additional_data.uploaderConfig.dataScope, "");
+
+        return new Uploader(
+            "imageuploader_" + this.contentType.id,
+            this.config.additional_data.uploaderConfig,
+            this.contentType.id,
+            this.contentType.dataStore,
+            initialImageValue,
+        );
+    };
+
+    /**
+     * Modify the options returned by the content type
+     *
+     * @returns {*}
+     */
+    Preview.prototype.retrieveOptions = function () {
+        var options = $super.retrieveOptions.call(this, arguments);
+
+        // Change option menu icons
+        options.remove.icon = "<i class='icon-admin-pagebuilder-error'></i>";
+
+        // Change tooltips
+        options.edit.title = "Open Editor";
+        // options.remove.title = "Remove";
+        // options.move.title = "Move";
+        // options.duplicate.title = "Duplicate";
+
+        // Remove menu options
+        // delete options.move;
+        // delete options.duplicate;
+        // delete options.edit;
+        // delete options.remove;
+
+        return options;
+    };
+
+    /**
+     * Allow various options of the content type to be modified from the stage
+     *
+     * @returns {*[]}
+     */
+    Preview.prototype.getToolbarOptions = function () {
+        return [
+            {
+                key: "text_align",
+                type: "select",
+                values: [{
+                    value: "left",
+                    label: "Left",
+                    icon: "icon-pagebuilder-align-left"
+                }, {
+                    value: "center",
+                    label: "Center",
+                    icon: "icon-pagebuilder-align-center"
+                }, {
+                    value: "right",
+                    label: "Right",
+                    icon: "icon-pagebuilder-align-right"
+                }]
+            },
+            {
+                key: "quote_css",
+                type: "select",
+                values: [{
+                    value: "black-quote",
+                    label: "Black",
+                    icon: "icon-pagebuilder-black-swatch"
+                }, {
+                    value: "blue-quote",
+                    label: "Blue",
+                    icon: "icon-pagebuilder-blue-swatch"
+                }, {
+                    value: "green-quote",
+                    label: "Green",
+                    icon: "icon-pagebuilder-green-swatch"
+                }, {
+                    value: "red-quote",
+                    label: "Red",
+                    icon: "icon-pagebuilder-red-swatch"
+                }, {
+                    value: "purple-quote",
+                    label: "Purple",
+                    icon: "icon-pagebuilder-purple-swatch"
+                }]
+            }
+        ];
+    };
+
+    return Preview;
+});
